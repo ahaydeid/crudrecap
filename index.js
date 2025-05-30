ce915f1f3806f0b385aaadcfb5a0b8421dc59738
ahaydeid <adi.hadi270@gmail.com>
2025-05-31 00:41:22 +0700

    Sabtu, 31 Mei 2025(belajar sesi 2 di jumat,30 mei 2025): CRUD udah lengkap semuanya, yeyy. Bisa tidur

--- index.js
+++ index.js
@@ -1,68 +1,81 @@
 import express from "express";
 const app = express();
 import bodyParser from "body-parser";
-const port = 3000;
 import db from "./connection.js";
-import response from "./response.js";
+// import response from "./response.js";
+import multer from "multer";
+import path from "path";
+const port = 3000;
+
+app.set("view engine", "hbs");
+// app.use(express.static("public"));
 
 app.use(bodyParser.json());
+app.use("/uploads", express.static("uploads"));
+app.use(express.urlencoded({ extended: false }));
 
-// Penulisan routes
-app.get("/", (req, res) => {
-  db.query("SELECT * FROM mahasiswa", (error, result) => {
-    response(200, result, "Get all data from table mahasiswa", res);
-  });
+// Tambilkan Data
+app.get("/", async (req, res) => {
+  const data = await db.query("SELECT * FROM mahasiswa ORDER BY id DESC");
+  res.render("form", { hasil: data.rows });
 });
 
-// app.get("/temu/:id", (req, res) => {
-//   const id = req.params.id;
-//   const sql = `SELECT * FROM mahasiswa WHERE nim = ${id}`;
-//   db.query(sql, (error, hasil) => {
-//     console.log(hasil.rows);
-//     res.send("Datanya sudah ketemu");
-//   });
-// });
+// Tambahkan Data
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads");
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + path.extname(file.originalname));
+  },
+});
+const upload = multer({ storage: storage });
 
-// app.get("/find", (req, res) =>{
-//   const sql = `SELECT * FROM mahasiswa WHERE nim = ${req.query.nim}`;
-//   db.query(sql, (error, result) => {
-//     if (error) throw error;
-//     response(200, result.rows, "Find Mahasiswa Name", res);
-//   });
-// });
+app.post("/submit", upload.single("foto"), (req, res) => {
+  const { name, nim, kelas, alamat } = req.body;
+  const foto = req.file ? req.file.filename : null;
+  const sql = `INSERT INTO mahasiswa (nama_lengkap, nim, kelas, alamat, foto) VALUES ('${name}', '${nim}', '${kelas}', '${alamat}', '${foto}')`;
+  db.query(sql, (err, result) => {
+    if (err) {
+      console.log("Gagal Tambahkan Data");
+    }
+  });
+  res.redirect("/");
+});
 
-// app.post("/tambah", (req, res) => {
-//   const { nim, namaLengkap, kelas, alamat } = req.body;
-//   const sql = `INSERT INTO mahasiswa (nim, nama_lengkap, kelas, alamat) VALUES ('${nim}', '${namaLengkap}', '${kelas}', '${alamat}')`;
-//   db.query(sql, (err, result) => {
-//     // Kalau error
-//     if (err) {
-//       console.error(err);
-//       return response(500, null, "error buangs", res);
-//     }
+// Edit Data
+// Alihkan Ke halaman edit
+app.get("/edit/:nim", (req, res) => {
+  const nim = req.params.nim;
+  const sql = `SELECT * FROM mahasiswa WHERE nim = '${nim}'`;
+  db.query(sql, (err, result) => {
+    console.log(result.rows);
+    res.render("edit", { data: result.rows[0] });
+  });
+});
+// Update data dengan data dari halaman edit
+app.post("/update", (req, res) => {
+  const { name, nim, kelas, alamat } = req.body;
+  const sql = `UPDATE mahasiswa SET nama_lengkap = '${name}', nim = '${nim}', kelas = '${kelas}', alamat = '${alamat}' WHERE nim = '${nim}'`;
+  db.query(sql, (err, result) => {
+    if (result.rowCount > 0) {
+      console.log("Update Berhasil");
+    }
+  });
+  res.redirect("/");
+});
 
-//     // Kalau berhasil
-//     if (result.rowCount > 0) {
-//       response(200, null, "Data berhasil ditambahkan", res);
-//     } else {
-//       response(400, null, "Data gagal ditambahkan", res);
-//     }
-//   });
-// });
-
-// app.put("/update", (req, res) => {
-//   const { nim, namaLengkap, kelas, alamat } = req.body;
-//   const sql = `UPDATE mahasiswa SET nama_lengkap = '${namaLengkap}', kelas = '${kelas}', alamat = '${alamat}' WHERE nim = '${nim}'`;
-
-//   db.query(sql, (err, result) => {
-//     if (err) response(500, "Invalid", "Error", res);
-//     if (result.rowCount > 0) {
-//       response(200, "OK", "Data updated successfully", res);
-//     } else {
-//       response(404, "Failed", "User not found", res);
-//     }
-//   });
-// });
+//Hapus Data
+app.delete("/:nim", (req, res) => {
+  const { nim } = req.params;
+  const sql = `DELETE FROM mahasiswa WHERE nim = ${nim}`;
+  db.query(sql, (error, fields) => {
+    if (fields.rowCount > 0) {
+      console.log("Data berhasil dihapus");
+    }
+  });
+  // res.redirect("/");
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
